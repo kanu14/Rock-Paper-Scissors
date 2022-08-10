cd02b7782b59b078afb4669ae9bbcd18453c31cf
kanu14 <mparkeh14@gmail.com>
2022-08-09 18:00:42 -0700

    added parameters to play game and score count

--- javascript.js
+++ javascript.js
@@ -1,5 +1,41 @@
 
-let choices = ['rock', 'paper', 'scissors']
-let getComputerChoice = choices[Math.floor(Math.random() * choices.length)];
+choices = ['rock', 'paper', 'scissors']
+function getComputerChoice () {
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
-console.log(getComputerChoice)
+const computerSelection = getComputerChoice();
+const playerSelection = prompt();
+playerScore = 0;
+compScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore ++
+    }   else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        compScore ++
+    }   else if (playerSelection == 'rock' && computerSelection == 'rock') {
+        console.log('Play Again')
+    }   else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        compScore ++
+    }   else if (playerSelection == 'paper' && computerSelection == 'paper') {
+        console.log('Play Again')
+    }   else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore ++
+    }   else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+        console.log('Play Again')
+    }   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore ++
+    }   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        compScore ++
+    }    
+}
+
+
+
+console.log(playRound(playerSelection,computerSelection));
+        
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playerScore);
+console.log(compScore);
