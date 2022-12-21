68daff243488a948292dcd14987ea2351e753160
kanu14 <mparkeh14@gmail.com>
2022-12-20 20:46:13 -0800

    still cant get round to play properly

--- javascript.js
+++ javascript.js
@@ -10,37 +10,35 @@ const computerSelection = getComputerChoice();
 
 let playerScore = 0;
 let compScore = 0;
+let playCount = 0
 
 function playRound(playerSelection, computerSelection) {
-    playerScore = 0;
-    compScore = 0;
+    playCount++
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore ++;
-        return ('You win! Rock beats scissors');
+        playerScore++;
+        return 'You win! Rock beats paper';
     }   else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        compScore ++;
-        return ('You lose! Rock beats paper');
+        compScore++;
+        return 'You lose! Rock beats paper';
     }   else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        compScore ++;
-        return ('You lose! Paper beats scissors');
+        compScore++;
+        return 'You lose! Paper beats scissors';
     }   else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore ++;
-        return ('You win! Paper beats rock')
+        playerScore++;
+        return 'You win! Paper beats rock';
     }   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore ++;
-        return ('You win! Scissors beats paper')
+        playerScore++;
+        return 'You win! Scissors beats paper';
     }   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        compScore ++;
-        return ('You lose! Scissors beats rock')
+        compScore++;
+        return 'You lose! Scissors beats rock';
     }   else if (playerScore == computerSelection) {
-        return ('Draw');
+        return 'Draw';
     }   
 }
 
-for (let i = 0; i < 5; i++) {
-    if (playerScore < 4 && compScore < 4 ){
-    playRound()
-    }
+for (let i = 0; playCount < 5; i++) {
+    playRound();
 }
 
 // function game () {
@@ -70,10 +68,12 @@ for (let i = 0; i < 5; i++) {
 // game ()
 
 
-console.log(playRound(playerSelection,computerSelection));
+// console.log(playRound(playerSelection,computerSelection));
         
 console.log(playerSelection);
 console.log(computerSelection);
+console.log(playRound);
 console.log(playerScore);
 console.log(compScore);
+console.log(playCount)
 
