694ed87ce031ddeb8d590f3c4e599504b592ff3b
kanu14 <mparkeh14@gmail.com>
2022-12-22 20:21:57 -0800

    game plays for 5 rounds with score described

--- javascript.js
+++ javascript.js
@@ -5,41 +5,59 @@ function getComputerChoice () {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-const playerSelection = prompt().toLowerCase();
-const computerSelection = getComputerChoice();
+// const playerSelection = prompt().toLowerCase();
+// const computerSelection = getComputerChoice();
 
 let playerScore = 0;
 let compScore = 0;
 let playCount = 0
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt().toLowerCase();
+    computerSelection = getComputerChoice();
+
+
+    // console.log(`playerSelection: ${playerSelection}; 
+    // computerSelection: ${computerSelection}`);
     playCount++
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerScore++;
-        return 'You win! Rock beats paper';
+        console.log( 'You win! Rock beats paper');
     }   else if (playerSelection == 'rock' && computerSelection == 'paper') {
         compScore++;
-        return 'You lose! Rock beats paper';
+        console.log( 'You lose! Rock beats paper');
     }   else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         compScore++;
-        return 'You lose! Paper beats scissors';
+        console.log( 'You lose! Paper beats scissors');
     }   else if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerScore++;
-        return 'You win! Paper beats rock';
+        console.log( 'You win! Paper beats rock');
     }   else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         playerScore++;
-        return 'You win! Scissors beats paper';
+        console.log( 'You win! Scissors beats paper');
     }   else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         compScore++;
-        return 'You lose! Scissors beats rock';
+        console.log( 'You lose! Scissors beats rock');
     }   else if (playerScore == computerSelection) {
-        return 'Draw';
+        console.log( 'Draw');
     }   
+    
 }
 
-for (let i = 0; playCount < 5; i++) {
+
+
+
+for (let i = 0; i < 5; i++) {
     playRound();
+    
 }
+
+    console.log(`Player Score = ${playerScore}; 
+    Computer Score = ${compScore}`);
+
+// console.log(playerScore);
+// console.log(compScore);
+
 
 // function game () {
 //     playRound
@@ -52,10 +70,6 @@ for (let i = 0; playCount < 5; i++) {
 // }
 
 
-// while (playerScore < 4 && compScore < 4) {
-//     playRound(playerSelection,computerSelection)
-// }
-
 // function game() {
 //     while (playerScore < 4 && compScore < 4) {
         
@@ -67,13 +81,11 @@ for (let i = 0; playCount < 5; i++) {
 
 // game ()
 
-
-// console.log(playRound(playerSelection,computerSelection));
         
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound);
-console.log(playerScore);
-console.log(compScore);
-console.log(playCount)
+// console.log(playerSelection);
+// console.log(computerSelection);
+// console.log(playRound);
+// console.log(playerScore);
+// console.log(compScore);
+// console.log(playCount)
 
